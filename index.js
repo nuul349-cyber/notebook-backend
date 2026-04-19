515dc3b065fec59a4466937c2909020d366c4bbf
Nuul <nuul@example.com>
2026-04-19 15:01:34 -0600

    frontend added and .gitignore

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 
-const cors = require('cors')
-
+app.use(express.static('dist'))
 app.use(cors())
 app.use(express.json())
 
