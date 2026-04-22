8f87d3c743cb89e164d3d981d46010de054171cb
Nuul <nuul@example.com>
2026-04-21 22:01:55 -0600

    Added note model and enviroment variables, can only query for all notes

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require('dotenv').config()
 const express = require('express')
-const app = express()
+const Note = require('./models/note')
 
+const app = express()
 app.use(express.static('dist'))
 app.use(express.json())
 
@@ -27,7 +29,9 @@ app.get('/', (request, response) => {
 })
 
 app.get('/api/notes', (request, response) => {
-  response.json(notes)
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
 })
 
 app.get('/api/notes/:id', (request, response) => {
@@ -74,7 +78,7 @@ app.post('/api/notes', (request, response) => {
     response.json(note)
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
