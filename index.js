56d38caf37e4a014b8f884f85864fed34cbe21c9
Nuul <nuul@example.com>
2026-04-22 13:58:27 -0600

    Update delete method to work with MongoDB

--- index.js
+++ index.js
@@ -49,19 +49,36 @@ app.get('/api/notes/:id', (request, response, next) => {
     .catch(error => next(error))
 })
 
-app.delete('/api/notes/:id', (request, response) => {
-  const id = request.params.id
-  notes = notes.filter(note => note.id !== id)
-
-  response.status(204).end()
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note
+    .findByIdAndDelete(request.params.id)
+    .then(result => {
+      console.log(result)
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
-const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => Number(n.id)))
-    : 0
-  return String(maxId + 1)
-}
+app.put('/api/notes/:id', (request, response, next) => {
+  const { content, important } = request.body
+
+  Note
+    .findById(request.params.id)
+    .then(note => {
+      if (!note) {
+        return response.status(404).end()
+      }
+
+      note.content = content
+      note.important = important
+
+      return note.save().then(updatedNote => {
+        response.json(updatedNote)
+      })
+    })
+    .catch(error => next(error))
+
+})
 
 app.post('/api/notes', (request, response) => {
     const body = request.body
