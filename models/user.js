90d0b748afa6382b8384f3e8aaaddc9512c6bcf9
Nuul <nuul@example.com>
2026-05-02 16:06:16 -0600

    Add userRouter to create new users. Update user model to make usernames unique and validate proper format. Add tests to POST of usersRouter

--- models/user.js
+++ models/user.js
@@ -1,7 +1,19 @@
 const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
-  username: String,
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    minlength: 3,
+    maxlength: 12,
+    validate: {
+      validator: v => {
+        return /^[A-Za-z0-9._-]+$/.test(v)
+      },
+      message: () => 'Only letters, numbers and this special sysmbols [. - _] are allowed'
+    }
+  },
   name: String,
   passwordHash: String,
   notes: [
