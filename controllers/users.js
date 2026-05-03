738a33ba348e600fea42027d496476e196e7a1d6
Nuul <nuul@example.com>
2026-05-03 17:19:12 -0600

    Update GET notes and users to show ther user and notes references using the mongoose.Document.prototype.populate()

--- controllers/users.js
+++ controllers/users.js
@@ -19,7 +19,7 @@ usersRouter.post('/', async (request, response) => {
 })
 
 usersRouter.get('/', async (request, response) => {
-  const users = await User.find({})
+  const users = await User.find({}).populate('notes', 'content important')
   response.json(users)
 })
 
